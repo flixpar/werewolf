d52eae9a19c1e896352e5b268bf0bcddc1296e0e
Felix Parker <flixpar@gmail.com>
2020-03-24 22:04:32 -0400

    implemented start of game

--- game.py
+++ game.py
@@ -15,14 +15,38 @@ roles = [
 def night(users):
 	print("night")
 
+	nusers = 1
+	nloaded = 0
+	nready = 0
+
+	@socketio.on("loaded")
+	def client_loaded():
+		nonlocal nloaded
+		nloaded += 1
+
+	while nloaded != nusers:
+		time.sleep(0.1)
+
+	socketio.emit("gameinfo", {
+		"nusers": nusers,
+		"usernames": users.username.tolist(),
+		"roles": roles
+	})
+
 	random.shuffle(roles)
 	users.role = roles[:len(users)]
-
-	time.sleep(2)
 
 	for i, user in users.iterrows():
 		print("sending:", user.role, "to", user.userid)
 		socketio.emit("distributeRole", {"role": user.role}, namespace="/"+user.userid)
+
+	@socketio.on("ready")
+	def client_ready():
+		nonlocal nready
+		nready += 1
+
+	while nready != nusers:
+		time.sleep(0.1)
 
 	socketio.emit("start")
 
--- static/client.js
+++ static/client.js
@@ -1,6 +1,8 @@
 console.log("Client started");
 
 var role;
+var gameinfo;
+
 var userid = document.getElementById("userid").dataset.userid;
 console.log("User ID: ".concat(userid));
 
@@ -9,13 +11,35 @@ mainArea = document.getElementById("main");
 var generalSocket = io();
 var playerSocket = io("/".concat(userid));
 
-generalSocket.on("start", _ => {
-	console.log("Game starting");
-	mainArea.innerHTML = "";
-	mainArea.innerHTML = "Your role is: ".concat(role);
+generalSocket.emit("loaded");
+
+generalSocket.on("gameinfo", gameinfoResponse => {
+	console.log(gameinfoResponse);
+	gameinfo = gameinfoResponse;
 });
 
 playerSocket.on("distributeRole", roleResponse => {
 	console.log(roleResponse);
 	role = roleResponse.role;
+
+	mainArea.innerHTML = "";
+
+	var roleElement = document.createElement("p");
+	roleElement.id = "role-display";
+	roleElement.innerHTML = "Your role is: ".concat(role);
+	mainArea.appendChild(roleElement);
+
+	var readyButton = document.createElement("button");
+	readyButton.id = "ready-button";
+	readyButton.innerHTML = "Ready";
+	readyButton.onclick = _ => {
+		generalSocket.emit("ready");
+	};
+	mainArea.appendChild(readyButton);
+});
+
+generalSocket.on("start", _ => {
+	console.log("Game starting");
+	mainArea.innerHTML = "";
+	mainArea.innerHTML = "Game starting...";
 });
